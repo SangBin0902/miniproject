7b80bb60bd42484b414f93b0ea4c7b193bac8eeb
HaeToon <gxg4205@gmail.com>
2024-08-08 10:40:16 +0900

    fixed

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Map<String, TodoList> participantTodoLists = new HashMap<>();
-        boolean isRunning = true;
-
-        while(isRunning) {
-            System.out.print("이름을 입력하세요 : ");
-            String name = scanner.nextLine();
-
-            TodoList todoList = participantTodoLists.get(name);
-            if (todoList == null) {
-                todoList = new TodoList();
-                participantTodoLists.put(name, todoList);
-                todoList.addTodo();
-            }
-
-            //todoList.printTodos();
-            System.out.println("1. 주간 보고서 작성, 2. 이메일 확인 및 응답, 3. 회의 준비, 4. 프로젝트 계획서 수정, 5. 팀 멤버와의 1:1 면담");
-            System.out.print("Title 번호: ");
-            int titleNum = scanner.nextInt();
-            scanner.nextLine();
-            todoList.complete(titleNum);
-            System.out.println("종료여부 : ");
-
-            isRunning = false;
-        }
+        TodoList todoList = new TodoList();
+        todoList.run();
     }
 }
--- src/TodoList.java
+++ src/TodoList.java
@@ -1,8 +1,11 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class TodoList {
     private List<Todo> todos;
+    Map<String, TodoList> participantTodoLists = new HashMap<>();
+    TodoList todoList;
+    Scanner sc = new Scanner(System.in);
+    private int processNum=1;
 
     public TodoList() {
         this.todos = new ArrayList<>();
@@ -34,6 +37,49 @@ public class TodoList {
     public void printTodos() {
         for(Todo todo : todos) {
             System.out.println(todo);
+        }
+    }
+    private void process1(){
+        System.out.print("이름을 입력하세요 : ");
+        String name = sc.nextLine();
+        todoList = participantTodoLists.get(name);
+        if (todoList == null) {
+            todoList = new TodoList();
+            participantTodoLists.put(name, todoList);
+            todoList.addTodo();
+        }
+        processNum++;
+    }
+    private void process2(){
+        System.out.println("1. 주간 보고서 작성, 2. 이메일 확인 및 응답, 3. 회의 준비, 4. 프로젝트 계획서 수정, 5. 팀 멤버와의 1:1 면담");
+        System.out.print("Title 번호: ");
+        int titleNum = sc.nextInt();
+        todoList.complete(titleNum);
+        processNum++;
+    }
+    private void process3(){
+        System.out.println("종료여부 : ");
+        String next = sc.nextLine();
+        if(next.equals("c")){
+            processNum=2;
+        }else if(next.equals("q")){
+            processNum=1;
+        }else if(next.equals("x")){
+            processNum=4;
+        }
+    }
+    public void run(){
+        while(true){
+            switch(processNum){
+                case 1 :
+                    process1();
+                case 2 :
+                    process2();
+                case 3 :
+                    process3();
+                case 4 :
+                    break;
+            }
         }
     }
 }
